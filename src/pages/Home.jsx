47ec3f8b47691e3e6f8b3480d13ad78a567bf956
Angela Diaz <angeldi846@gmail.com>
2023-01-16 21:14:58 -0500

    update responsive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,7 @@ import Footer from '../components/layouts/Footer'
 
 import Filter from '../components/common/Filter';
 import SkillCircle from '../components/common/SkillCircle';
-
+import CardProject from '../components/common/CardProject'
 import Projects from '../data/projects'
 
 import RobotAbout from '../assets/img/robot-about.png'
@@ -19,12 +19,14 @@ import RobotPlan from '../assets/img/robot-plan.png'
 import RobotDesign from '../assets/img/robot-design.png'
 import RobotCode from '../assets/img/robot-coding.png'
 import RobotDeploy from '../assets/img/robot-deploy-2.png'
-import CardProject from '../components/common/CardProject'
-
+import SkillsDesign from '../assets/img/skills-design.png'
+import SkillsCode from '../assets/img/skills-code.png'
 import BgFooter from '../assets/img/background-footer.png'
 
+const SkillsImage = require.context("../assets/img/skills", true);
 
 function Home () {
+
 
   const [search, setSearch] = useState("");
     const [allProjects, setAllProjects] = useState(Projects);
@@ -84,7 +86,7 @@ function Home () {
     }, [category]);
 
   return (
-    <>
+    <> 
       <section className='banner'>
         <div className='banner-content'>
           <h1>Hi, I'm Angela</h1>
@@ -109,6 +111,59 @@ function Home () {
         <SkillCircle image={RobotDesign} name="Design"/>
         <SkillCircle image={RobotCode} name="Code"/>
         <SkillCircle image={RobotDeploy} name="Deploy" classImage="robot-deploy"/>
+      </section>
+
+      <section className='skills'>
+        <article>
+          <div className='container-robot'>
+            <img src={SkillsDesign} alt="Robot design skills"/>
+          </div>
+          <div className='list-skills'>
+              <div className='card-skill'>
+                <img src={SkillsImage(`./figma.png`)} alt="" />
+                <p>FIGMA</p>
+              </div>
+              <div className='card-skill'>
+                <img src={SkillsImage(`./ilustrator.png`)} alt="" />
+                <p>A. ILUSTRATOR</p>
+              </div>
+          </div>
+        </article>
+        <article>
+          <div className='container-robot'>
+            <img src={SkillsCode} alt="Robot code skills"/>
+          </div>
+          <div className='list-skills'>
+              <div className='card-skill'>
+                <img src={SkillsImage(`./css.png`)} alt=""/>
+                <p>CSS</p>
+              </div>
+              <div className='card-skill'>
+                <img src={SkillsImage(`./html.png`)} alt=""/>
+                <p>HTML</p>
+              </div>
+              <div className='card-skill'>
+                <img src={SkillsImage(`./js.png`)} alt=""/>
+                <p>JAVASCRIPT</p>
+              </div>
+              <div className='card-skill'>
+                <img src={SkillsImage(`./sass.png`)} alt=""/>
+                <p>SASS</p>
+              </div>
+              <div className='card-skill'>
+                <img src={SkillsImage(`./bootstrap.png`)} alt=""/>
+                <p>BOOTSTRAP</p>
+              </div>
+              <div className='card-skill'>
+                <img src={SkillsImage(`./react.png`)} alt=""/>
+                <p>React</p>
+              </div>
+              <div className='card-skill'>
+                <img src={SkillsImage(`./python.png`)} alt=""/>
+                <p>Python</p>
+              </div>
+          </div>
+        </article>
       </section>
 
       <h1 className='title-separators'>Web developer portfolio</h1>
